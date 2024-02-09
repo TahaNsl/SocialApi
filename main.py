7580bfaf9ba7e3b699de097f9b970c1aa13e726a
Taha Nasrollahi <tahanasrollahii@gmail.com>
2024-02-09 18:34:51 +0330

    include routers

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from db.models import Base
+from db.database import engine
+from routers import user, post, comment
+from auth import authentication
+
 
 app = FastAPI()
+app.include_router(user.router)
+app.include_router(post.router)
+app.include_router(comment.router)
+app.include_router(authentication.router)
+
+app.mount("/uploaded_files",
+          StaticFiles(directory="uploaded_files"),
+          name="uploaded_files")
+
+Base.metadata.create_all(engine)
 
 @app.get("/")
 def home():
